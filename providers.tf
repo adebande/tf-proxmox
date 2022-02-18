3d4d75b7742dfdb6e41e33d3f3c9342b810c2dbd
adebande <63793809+adebande@users.noreply.github.com>
2022-02-18 14:20:31 +0100

    add api auth variables

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url          = "https://192.168.0.246:8006/api2/json"
-  pm_api_token_id     = "root@pam!terraform"
-  pm_api_token_secret = "145a2cd0-6e75-4611-beb4-1954bd76188e"
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,18 @@
+variable "pm_api_url" {
+  type = string
+}
+
+
+variable "pm_api_token_id" {
+  type = string
+}
+
+variable "pm_api_token_secret" {
+  type = string
+}
+
+
+
 variable "name" {
   type    = string
   default = "ubuntu-tf"
